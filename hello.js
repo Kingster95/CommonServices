097424b5c6e22e38ee63efc34aafdeacea5e720c
Ortan Mihai <mihai.ortan@gmail.com>
2023-09-28 14:53:55 +0300

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,1 +1,9 @@
-console.log("Hello World");
+// In hello.js
+function sayHello() {
+  console.log("Hello");
+}
+
+// In your index.html or a separate JavaScript file
+window.onload = function() {
+  sayHello(); // Call the sayHello function when the page loads
+};
